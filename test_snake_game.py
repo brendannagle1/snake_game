a40dddeafeb882bf3118cdc61cfb252b426f42c7
BRENDAN NAGLE <brendannagle1@gmail.com>
2024-07-18 05:00:21 -0500

    - Removed Global variable calls and replaced with local calls - Removed threading and broke the controls
    TODO:
    - Replace native windows controls calls with keyboard library
    - Create Requirements.txt file
    - Create more unittests
    - Create FPS lock and remove time.sleep calls and change to non blocking
      perf_counter calls
     -Main loop should only check for keyboard inputs and update
     imshow continuously.
     - Render with a sepearate threadpotentially?

---
+++ test_snake_game.py
@@ -0,0 +1,25 @@
+import unittest
+import snake_game
+from unittest.mock import patch
+
+class TestSnakeGame(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
+    def test_gentarget(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
